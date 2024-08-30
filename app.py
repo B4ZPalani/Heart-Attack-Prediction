8a60f0b028042bd455b32c657e4b75d5c63b4b43
B4ZPalani <160098125+B4ZPalani@users.noreply.github.com>
2024-08-30 11:58:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -30,24 +30,7 @@ def main():
                  width=150)
         submit = st.button("Predict")
     with col2:
-        st.markdown("""
-        Did you know that machine learning models can help you
-        predict heart disease pretty accurately? In this app, you can
-        estimate your chance of heart disease (yes/no) in seconds!
-        
-        Here, a logistic regression model using an undersampling technique
-        was constructed using survey data of over 300k US residents from the year 2020.
-        This application is based on it because it has proven to be better than the random forest
-        (it achieves an accuracy of about 80%, which is quite good).
-        
-        To predict your heart disease status, simply follow the steps bellow:
-        1. Enter the parameters that best describe you;
-        2. Press the "Predict" button and wait for the result.""");
-        
-
-
-
-
+    
     st.sidebar.title("Feature Selection")
     st.sidebar.image("images/heart-sidebar.png", width=100)
 
