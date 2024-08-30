3fefe2190dd814313837ac968f56c99b4912d90f
B4ZPalani <160098125+B4ZPalani@users.noreply.github.com>
2024-08-30 11:53:30 +0530

    Update streamlit_app.py
    Basic Web View code

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,56 @@
 import streamlit as st
+import pandas as pd
+import polars as pl
+import numpy as np
+import pickle
+import sklearn
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+
+
+
+def main():
+    @st.cache(persist=True)
+   
+
+
+    st.set_page_config(
+        page_title="Heart Disease Prediction App",
+        page_icon="images/heart-fav.png"
+    )
+
+    st.title("Heart Disease Prediction")
+    st.subheader("Are you wondering about the condition of your heart? "
+                 "This app will help you to diagnose it!")
+
+    col1, col2 = st.columns([1, 3])
+
+    with col1:
+        st.image("images/doctor.png",
+                 caption="I'll help you diagnose your heart health! - Dr. Logistic Regression",
+                 width=150)
+        submit = st.button("Predict")
+    with col2:
+        st.markdown("""
+        Did you know that machine learning models can help you
+        predict heart disease pretty accurately? In this app, you can
+        estimate your chance of heart disease (yes/no) in seconds!
+        
+        Here, a logistic regression model using an undersampling technique
+        was constructed using survey data of over 300k US residents from the year 2020.
+        This application is based on it because it has proven to be better than the random forest
+        (it achieves an accuracy of about 80%, which is quite good).
+        
+        To predict your heart disease status, simply follow the steps bellow:
+        1. Enter the parameters that best describe you;
+        2. Press the "Predict" button and wait for the result.
+        
+
+
+
+
+    st.sidebar.title("Feature Selection")
+    st.sidebar.image("images/heart-sidebar.png", width=100)
+
+ 
+if __name__ == "__main__":
+    main()
