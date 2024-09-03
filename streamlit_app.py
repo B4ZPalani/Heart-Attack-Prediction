eeda701e68db607c01d092b9a718dd3a58b71b18
Palanikumar Manoharan <palanikumar.manoharan@byteforza.com>
2024-09-03 11:07:07 +0530

    Feature Input UI implementation

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,68 @@ import numpy as np
 import pickle
 import sklearn
 
+
+
+
+def user_input_features() -> pd.DataFrame:
+        race = st.sidebar.selectbox("Race", options=(race for race in heart.Race.unique()))
+        sex = st.sidebar.selectbox("Sex", options=(sex for sex in heart.Sex.unique()))
+        age_cat = st.sidebar.selectbox("Age category",
+                                       options=(age_cat for age_cat in heart.AgeCategory.unique()))
+        bmi_cat = st.sidebar.selectbox("BMI category",
+                                       options=(bmi_cat for bmi_cat in heart.BMICategory.unique()))
+        sleep_time = st.sidebar.number_input("How many hours on average do you sleep?", 0, 24, 7)
+        gen_health = st.sidebar.selectbox("How can you define your general health?",
+                                          options=(gen_health for gen_health in heart.GenHealth.unique()))
+        phys_health = st.sidebar.number_input("For how many days during the past 30 days was"
+                                              " your physical health not good?", 0, 30, 0)
+        ment_health = st.sidebar.number_input("For how many days during the past 30 days was"
+                                              " your mental health not good?", 0, 30, 0)
+        phys_act = st.sidebar.selectbox("Have you played any sports (running, biking, etc.)"
+                                        " in the past month?", options=("No", "Yes"))
+        smoking = st.sidebar.selectbox("Have you smoked at least 100 cigarettes in"
+                                       " your entire life (approx. 5 packs)?)",
+                                       options=("No", "Yes"))
+        alcohol_drink = st.sidebar.selectbox("Do you have more than 14 drinks of alcohol (men)"
+                                             " or more than 7 (women) in a week?", options=("No", "Yes"))
+        stroke = st.sidebar.selectbox("Did you have a stroke?", options=("No", "Yes"))
+        diff_walk = st.sidebar.selectbox("Do you have serious difficulty walking"
+                                         " or climbing stairs?", options=("No", "Yes"))
+        diabetic = st.sidebar.selectbox("Have you ever had diabetes?",
+                                        options=(diabetic for diabetic in heart.Diabetic.unique()))
+        asthma = st.sidebar.selectbox("Do you have asthma?", options=("No", "Yes"))
+        kid_dis = st.sidebar.selectbox("Do you have kidney disease?", options=("No", "Yes"))
+        skin_canc = st.sidebar.selectbox("Do you have skin cancer?", options=("No", "Yes"))
+
+        features = pd.DataFrame({
+            "PhysicalHealth": [phys_health],
+            "MentalHealth": [ment_health],
+            "SleepTime": [sleep_time],
+            "BMICategory": [bmi_cat],
+            "Smoking": [smoking],
+            "AlcoholDrinking": [alcohol_drink],
+            "Stroke": [stroke],
+            "DiffWalking": [diff_walk],
+            "Sex": [sex],
+            "AgeCategory": [age_cat],
+            "Race": [race],
+            "Diabetic": [diabetic],
+            "PhysicalActivity": [phys_act],
+            "GenHealth": [gen_health],
+            "Asthma": [asthma],
+            "KidneyDisease": [kid_dis],
+            "SkinCancer": [skin_canc]
+        })
+
+        return features
+
+
+
+
+
+
+
+
 # Set page configuration outside of any function
 st.set_page_config(
     page_title="ByteForza - Heart Attack Prediction App",
