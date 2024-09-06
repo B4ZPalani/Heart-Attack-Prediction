86d7e8f6ae61d508e2715866e25edc1dec1d8e41
Palanikumar Manoharan <palanikumar.manoharan@byteforza.com>
2024-09-06 02:45:45 +0000

    latest update

--- streamlit_app.py
+++ streamlit_app.py
@@ -55,8 +55,8 @@ def readDataset():
     return df
 
 def basicCleansing(df):
-    st.write("before cleansing")
-    st.write(df.head(100))
+    #st.write("before cleansing")
+    #st.write(df.head(100))
     df['Smoking'] = pd.Series(np.where(df['Smoking'] == 'Yes', 1, 0))
     df['AlcoholDrinking'] = pd.Series(np.where(df['AlcoholDrinking'] == 'Yes', 1, 0))
     df['DiffWalking'] = pd.Series(np.where(df['DiffWalking'] == 'Yes', 1, 0))
@@ -99,17 +99,43 @@ def trainLogisticRegressionModel(X_train, y_train,X_test, y_test):
     # Initialize and train the Logistic Regression model
     from sklearn.linear_model import LogisticRegression
     lr_model = LogisticRegression(random_state=42, max_iter=1000)
-    st.write(X_train.head(100))
+    #st.write(X_train.head(100))
     lr_model.fit(X_train, y_train)
     y_lr_pred = lr_model.predict(X_test)
-    st.write(y_lr_pred.shape)
+    st.write("LogisticRegression")
+    st.write(y_lr_pred)
+
+    from sklearn.metrics import accuracy_score
+    accuracy = accuracy_score(y_test, y_lr_pred)
+    st.write("LogisticRegression Accuracy Score")
+    st.write(f'Accuracy: {accuracy:.2f}')
+
     # unique, counts = np.unique(y_lr_pred, return_counts=True)
     # count_dict = dict(zip(unique, counts))
     # st.write(count_dict)
     return lr_model
 
+def trainRandomForestModel(X_train, y_train,X_test, y_test):
+    # Initialize and train the Logistic Regression model
+    from sklearn.ensemble import RandomForestClassifier
+    rf_model = RandomForestClassifier(n_estimators=100, random_state=42)
+    #st.write(X_train.head(100))
+    rf_model.fit(X_train, y_train)
+    y_rf_pred = rf_model.predict(X_test)
+    st.write("RandomForestClassifier")
+    st.write(y_rf_pred)
+
+    from sklearn.metrics import accuracy_score
+    accuracy = accuracy_score(y_test, y_rf_pred)
+    st.write("RandomForestClassifier Accuracy Score")
+    st.write(f'Accuracy: {accuracy:.2f}')
+    # unique, counts = np.unique(y_lr_pred, return_counts=True)
+    # count_dict = dict(zip(unique, counts))
+    # st.write(count_dict)
+    return rf_model
+
 def createUIElements():
-    st.title("Heart Attack Prediction1")
+    st.title("Heart Attack Prediction")
     st.subheader("Concerned about your heart health? This app is here to help you assess your risk and take proactive steps to safeguard your heart!")
     # Display the image and prediction button
     col1, col2 = st.columns([1, 3])
@@ -145,23 +171,44 @@ def createUIElements():
 def predictionByUserInput(user_input):
     df = readDataset()
     X_train, X_test, y_train, y_test = splitTestTrainData(df)
-    model = trainLogisticRegressionModel(X_train, y_train,X_test, y_test)
-    
     cleaned_df = basicCleansing(user_input)
-    prediction = model.predict(cleaned_df)
-    prediction_prob = model.predict_proba(cleaned_df)
-    if prediction == 0:
+
+
+    lr_model = trainLogisticRegressionModel(X_train, y_train,X_test, y_test)
+    lr_prediction = lr_model.predict(cleaned_df)
+    Lr_prediction_prob = lr_model.predict_proba(cleaned_df)
+
+    rf_model = trainRandomForestModel(X_train, y_train,X_test, y_test)
+    rf_prediction = rf_model.predict(cleaned_df)
+    fr_prediction_prob = rf_model.predict_proba(cleaned_df)
+
+    st.subheader("Logistic Regression Prediction")
+    if lr_prediction == 0:
             st.markdown(f"**The probability that you'll have"
-                        f" heart disease is {round(prediction_prob[0][1] * 100, 2)}%."
+                        f" heart disease is {round(Lr_prediction_prob[0][1] * 100, 2)}%."
                         f" You are healthy!**")
             st.image("images/heart-okay.jpg",
                      caption="Your heart seems to be okay! - Dr. ByteForza Regression")
     else:
             st.markdown(f"**The probability that you will have"
-                        f" heart disease is {round(prediction_prob[0][1] * 100, 2)}%."
+                        f" heart disease is {round(Lr_prediction_prob[0][1] * 100, 2)}%."
                         f" It sounds like you are not healthy.**")
             st.image("images/heart-bad.jpg",
                      caption="I'm not satisfied with the condition of your heart! - Dr. ByteForza Regression")
+            
+    st.subheader("Random Forest Prediction")
+    if rf_prediction == 0:
+            st.markdown(f"**The probability that you'll have"
+                        f" heart disease is {round(fr_prediction_prob[0][1] * 100, 2)}%."
+                        f" You are healthy!**")
+            st.image("images/heart-okay.jpg",
+                     caption="Your heart seems to be okay! - Dr. ByteForza Forest")
+    else:
+            st.markdown(f"**The probability that you will have"
+                        f" heart disease is {round(fr_prediction_prob[0][1] * 100, 2)}%."
+                        f" It sounds like you are not healthy.**")
+            st.image("images/heart-bad.jpg",
+                     caption="I'm not satisfied with the condition of your heart! - Dr. ByteForza Forest")
 
 def main():
     createUIElements()
